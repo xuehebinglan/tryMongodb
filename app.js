2a290210e0fe6a85d40e51d16261f6505da0dcaf
xuehebinglan <785529534@qq.com>
2019-05-07 20:27:23 +0800

    可以给mongodb插入数据了

--- app.js
+++ app.js
@@ -5,22 +5,23 @@ const assert = require('assert');
 const url = 'mongodb://localhost:27017';
 
 // Database Name
-const dbName = 'myproject';
+const dbName = 'myproject2';
 
 const insertDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('documents');
-    // Insert some documents
-    collection.insertMany([
-        {a : 1}, {a : 2}, {a : 3}
-    ], function(err, result) {
-        assert.equal(err, null);
-        assert.equal(3, result.result.n);
-        assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
-        callback(result);
-    });
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {c : {a:1}},{c : {a:1}},{c : {a:1}}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
 }
+
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, client) {
@@ -29,5 +30,7 @@ MongoClient.connect(url, function(err, client) {
 
   const db = client.db(dbName);
 
-  client.close();
+  insertDocuments(db, function() {
+    client.close();
+  });
 });
